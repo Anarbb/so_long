e8b0f3278ece9abd0b69d05c8b75748e6b4cd384
Anas <anas@arbaoui.me>
2022-12-26 16:20:31 +0100

    OK.

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: aarbaoui <aarbaoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 13:39:16 by aarbaoui          #+#    #+#             */
-/*   Updated: 2022/12/26 15:02:06 by aarbaoui         ###   ########.fr       */
+/*   Updated: 2022/12/26 16:19:06 by aarbaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int	main(int argc, char **argv)
 		if (!init_map(game, argv[1]))
 			exit_game(game, "Error: Invalid map\n", 1);
 		game->win_ptr = mlx_new_window(game->mlx_ptr, game->map->width,
-				game->map->height + 64, "so_long");
+				game->map->height + SS * 2, "so_long");
 		init_game(game);
 		mlx_loop_hook(game->mlx_ptr, update, game);
 		mlx_hook(game->win_ptr, 2, 0, move_player, game);
